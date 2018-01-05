06f8207fd267be360d3c6d115377271827a596e1
isoden <isoda@maboroshi.biz>
2018-01-06 08:01:30 +0900

    chore: add license comment

--- src/app/unless/unless.module.ts
+++ src/app/unless/unless.module.ts
@@ -1,3 +1,12 @@
+/**
+ * @isoden/ngx-unless - An Angular module to add unless syntax to template.
+ *
+ * https://github.com/isoden/ngx-unless
+ *
+ * Copyright (c) 2018 Yu Isoda
+ * Licensed under the MIT license. https://isoden.mit-license.org
+ */
+
 import { NgModule } from '@angular/core';
 import { UnlessDirective } from './unless.directive';
 
